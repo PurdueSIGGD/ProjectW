bbb77ba41a63c6db6ff8aa6b31420230f524a893
Andrew Lonsway <alonswa@purdue.edu>
2017-10-09 04:02:58 -0400

    It keeps forgetting some files

--- Assets/C#/PlayerScripts/PlayerAbilities/SpeedBoost/Ability_SpeedBoost.cs
+++ Assets/C#/PlayerScripts/PlayerAbilities/SpeedBoost/Ability_SpeedBoost.cs
@@ -2,14 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class Ability_SpeedBoost : CooldownAbility {
     public float percentageGain = 0.3f;
     public float duration = 3f;
     public GameObject startSound, endSound;
+    [SyncVar]
+    bool used = false;
+    bool hasUsed = false;
 
     public override void use_CooledDown() {
-        Effect_SpeedBoost boost = this.gameObject.AddComponent<Effect_SpeedBoost>();
-        boost.Initialize(myBase.myMovement, percentageGain, duration, startSound, endSound);
+        used = true;
+    }
+    // Oh, so unity doesn't like to have two updates in the same compnent, likea parent class. So that wouldn't work.
+    void LateUpdate() {
+        if (used && !hasUsed) {
+            hasUsed = true;
+            Effect_SpeedBoost boost = this.gameObject.AddComponent<Effect_SpeedBoost>();
+            boost.Initialize(myBase.myMovement, percentageGain, duration, startSound, endSound);
+        }
+    }
+
+    public override void use_CanUse() {
+        used = false;
+        hasUsed = false;
     }
 }
--- Assets/C#/PlayerScripts/PlayerAbilities/SpeedBoost/Effect_SpeedBoost.cs
+++ Assets/C#/PlayerScripts/PlayerAbilities/SpeedBoost/Effect_SpeedBoost.cs
@@ -2,21 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class Effect_SpeedBoost : NetworkBehaviour {
     public PlayerMovement target;
     public float speedIncreasePercentage;
     private float endTime;
-    private AudioClip start, end;
-    private GameObject endSound;
+    GameObject startSound, endSound;
 
 	public void Initialize(PlayerMovement target, float percentage, float duration, GameObject startSound, GameObject endSound) {
         this.target = target;
         speedIncreasePercentage = percentage;
         endTime = Time.time + duration;
         target.sRunSpeedModifier += speedIncreasePercentage;
-        CmdSpawnBoost(startSound);
+        this.startSound = startSound;
         this.endSound = endSound;
+        CmdSpawnBoost(this.startSound);
+        //EditorApplication.isPaused = true;
     }
 
 	// Update is called once per frame
@@ -30,6 +34,7 @@ public class Effect_SpeedBoost : NetworkBehaviour {
     //[Command]
     public void CmdSpawnBoost(GameObject toInstantiate) {
         GameObject result = GameObject.Instantiate(toInstantiate, target.transform);
+        result.transform.localPosition = Vector3.zero; // Center on parent
         //NetworkServer.Spawn(result);
     }
 }
