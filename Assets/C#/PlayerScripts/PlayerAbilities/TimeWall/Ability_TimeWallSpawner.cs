5e257687852053ba2264e1e4d50cf81ed36b3f69
Clayton <cjdetke@yahoo.com>
2017-11-09 18:54:44 -0500

    Only added time wall ability, no other changes

---
+++ Assets/C#/PlayerScripts/PlayerAbilities/TimeWall/Ability_TimeWallSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability_TimeWallSpawner : Ability_ObjectSpawner {
+    public override void OnSpellSpawned(GameObject spawn)
+    {
+        TimeWall t;
+        if (t = spawn.GetComponent<TimeWall>())
+        {
+            t.sourcePlayer = this.gameObject;
+            t.cooldown = this.cooldown;
+        }
+    }
+}
---
+++ Assets/C#/PlayerScripts/PlayerAbilities/TimeWall/TimeWall.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeWall : MonoBehaviour {
+
+    public GameObject sourcePlayer;
+    public float cooldown;
+
+    private ArrayList hitByThese;
+    private ArrayList velocities;
+	void Start () {
+        Destroy(this.gameObject, cooldown);
+        hitByThese = new ArrayList();
+        velocities = new ArrayList();
+	}
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.GetComponentInParent<IHittable>() != null)
+            return;
+        if (col.isTrigger)
+        {
+            Rigidbody r;
+            if((r = col.GetComponent<Rigidbody>()) != null)
+            {
+                hitByThese.Add(r);
+                velocities.Add(r.velocity);
+                r.AddForce(-(r.velocity), ForceMode.VelocityChange);
+            }
+            return;
+        }
+    }
+
+    void OnDestroy()
+    {
+        for(int i=0; i<hitByThese.Count; i++)
+        {
+            Rigidbody r = (Rigidbody) hitByThese[i];
+            if(r != null)
+                r.AddForce(-((Vector3) velocities[i]), ForceMode.VelocityChange);
+        }
+    }
+}
