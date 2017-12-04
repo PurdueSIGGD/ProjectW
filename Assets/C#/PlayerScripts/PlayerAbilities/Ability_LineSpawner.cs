a3f0d0367a6360d52e8975e79df1938105305946
yang132 <31993113+yang132@users.noreply.github.com>
2017-12-03 21:29:03 -0500

    PointSpawner and LineSpawner
    Implemented pointSpawner and lineSpawner as abilites on the flame player

---
+++ Assets/C#/PlayerScripts/PlayerAbilities/Ability_LineSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability_LineSpawner : Ability_PointSpawner {
+
+	public int numSpawns;
+	public float delay;
+
+	public override void SpawnSpell(PlayerComponent.Buf data)
+	{
+		float incr = spawnRange / numSpawns;
+
+		StartCoroutine(spawnDelay(data, incr));
+
+
+
+	}
+
+	public IEnumerator spawnDelay(PlayerComponent.Buf data, float incr)
+	{
+		//GetComponent(Ability_LineSpawner).spawnRange = i * incr;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		ray = new Ray(ray.origin, new Vector3(ray.direction.x, 0, ray.direction.z));
+		for (int i = 1; i <= numSpawns; i++)
+		{
+			SpawnSpell(data, i * incr, ray);
+			yield return new WaitForSeconds(delay);
+		}
+	}
+}
---
+++ Assets/C#/PlayerScripts/PlayerAbilities/Ability_PointSpawner.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability_PointSpawner : Ability_ObjectSpawner {
+	public float spawnRange;
+
+	public override void SpawnSpell(PlayerComponent.Buf data)
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		SpawnSpell(data, spawnRange, ray);
+	}
+
+
+	public void SpawnSpell(PlayerComponent.Buf data, float rangeVaried, Ray ray) {
+		Vector3 spawnAngle = data.vectorList[0];
+		Vector3 spawnPosition = data.vectorList[1];
+		Debug.Log("varied " + rangeVaried);
+
+		RaycastHit hit;
+		GameObject spawn;
+
+		if (Physics.Raycast (ray, out hit, rangeVaried)) {
+
+			//	Debug.DrawRay(spawnPoint.position + spawnOffset, spawnAngle, Color.green, 10);
+			// Spawn our spell in the place the server told us
+			// However if we are the client, we don't wait for that luxury.
+			spawn = collided(hit);
+
+
+
+		} 
+		else {
+			spawn = maxRangeReached (ray, rangeVaried);
+		}
+
+		Rigidbody r;
+		if (r = spawn.GetComponent<Rigidbody>())
+		{
+			r.AddForce(spawnAngle * spawnSpeed);
+		}
+		OnSpellSpawned(spawn);
+	}
+
+
+	public virtual GameObject collided(RaycastHit hit)
+	{
+		GameObject spawn;
+
+		spawn = GameObject.Instantiate(itemToSpawn, hit.point, Quaternion.identity);
+
+		return spawn;
+	}
+
+	public virtual GameObject maxRangeReached(Ray ray, float rangeVaried)
+	{
+		GameObject spawn;
+		//spawn = GameObject.Instantiate (itemToSpawn, ray.origin + ray.direction * spawnRange, Quaternion.identity);
+
+
+		Ray drop = new Ray(ray.origin + ray.direction * rangeVaried, -Vector3.up);
+		RaycastHit hit;
+
+		Physics.Raycast (drop, out hit, rangeVaried * 3);
+
+		spawn = GameObject.Instantiate (itemToSpawn, hit.point, Quaternion.identity);
+
+
+		return spawn;
+	}
+
+	public override void OnSpellSpawned(GameObject spawn)
+	{
+		Explosion e;
+		if (e = spawn.GetComponent<Explosion>())
+		{
+			e.sourcePlayer = this.gameObject;
+		}
+	}
+
+}
