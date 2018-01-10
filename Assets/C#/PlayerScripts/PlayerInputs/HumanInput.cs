78068c92b2388ae3d36b2cf14fac048de91d373d
Chau Minh Nguyen <cnguyenm@purdue.edu>
2018-01-09 19:47:46 -0500

    Start Chatbox
    * Simple GUI
    * simple chat animation

--- Assets/C#/PlayerScripts/PlayerInputs/HumanInput.cs
+++ Assets/C#/PlayerScripts/PlayerInputs/HumanInput.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class HumanInput : PlayerInput {
     public float sensitivityy = 0.05f;
 	public override PlayerInput.InputData getData() {
-        InputData data = new InputData();
+        InputData data  = new InputData();
         data.horizontal = Input.GetAxis("Horizontal");
-        data.vertical = Input.GetAxis("Vertical");
-        data.mouseX = Input.GetAxis("Mouse X") * sensitivityy;
-        data.mouseY = Input.GetAxis("Mouse Y") * sensitivityy;
-        data.pause = Input.GetAxis("Pause") > 0;
-        data.jump = Input.GetAxis("Jump") > 0;
+        data.vertical   = Input.GetAxis("Vertical");
+        data.mouseX     = Input.GetAxis("Mouse X") * sensitivityy;
+        data.mouseY     = Input.GetAxis("Mouse Y") * sensitivityy;
+        data.pause      = Input.GetAxis("Pause") > 0;
+        data.jump       = Input.GetAxis("Jump") > 0;
         data.useAbilities = new bool[ABILITY_INPUTS];
         // ability list starts at 1
         for (int i = 1; i <= PlayerInput.ABILITY_INPUTS; i++) {
