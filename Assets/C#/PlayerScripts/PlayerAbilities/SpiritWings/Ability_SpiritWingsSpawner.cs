11865a0da485537d888ea40601fbc3a34179046e
Clayton <cjdetke@yahoo.com>
2018-01-23 19:29:38 -0500

    Created initial Spirit Wings ability
    No changes to anything else, only created an ability and scripts.

---
+++ Assets/C#/PlayerScripts/PlayerAbilities/SpiritWings/Ability_SpiritWingsSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability_SpiritWingsSpawner : Ability_ObjectSpawner {
+
+    public float spellDuration = 2;
+    public float boostMultiplier = 1f;
+
+    public override void OnSpellSpawned(GameObject spawn)
+    {
+        SpiritWings sw;
+        if (sw = spawn.GetComponent<SpiritWings>())
+        {
+            sw.sourcePlayer = this.gameObject;
+            sw.StartSpiritWings(this, spellDuration);
+            sw.transform.SetParent(this.gameObject.transform);
+        }
+    }
+    public void boost()
+    {
+        this.gameObject.GetComponent<Rigidbody>().AddForce(aimAngle.rotation * new Vector3(0,0,boostMultiplier), ForceMode.VelocityChange);
+    }
+}
---
+++ Assets/C#/PlayerScripts/PlayerAbilities/SpiritWings/SpiritWings.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritWings : MonoBehaviour {
+
+    public GameObject sourcePlayer;
+    public HitArguments.DamageType damageType;
+    private float previousBoost;
+    private float coolDown = .02f;
+    private Ability_SpiritWingsSpawner ability;
+
+    public void StartSpiritWings(Ability_SpiritWingsSpawner ability, float spellDuration) {
+        this.ability = ability;
+        Destroy(this.gameObject, spellDuration);
+        previousBoost = Time.time;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponentInParent<IHittable>() != null)
+            return;
+        if (col.isTrigger)
+        {
+            Rigidbody r;
+            if ((r = col.GetComponent<Rigidbody>()) != null)
+            {
+                
+                //r.AddForce(-(r.velocity), ForceMode.VelocityChange);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(Time.time - previousBoost > coolDown)
+        {
+            previousBoost = Time.time;
+            ability.boost();
+        }
+    }
+}
