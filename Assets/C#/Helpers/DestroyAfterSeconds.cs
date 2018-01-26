c8e780a79e782e2baac7c60a5205d564d518cadd
cnguyenm <cnguyenm@purdue.edu>
2018-01-25 19:14:40 -0500

    Healing circle working

---
+++ Assets/C#/Helpers/DestroyAfterSeconds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterSeconds : MonoBehaviour {
+
+    public float lifeTime;  // in seconds
+
+	// Use this for initialization
+	void Start () {
+        Invoke("DestroyMe", lifeTime);
+	}
+	
+	void DestroyMe()
+    {
+        Destroy(this.gameObject);
+    }
+}
