0cbb4466fc06c7961929178f159ae287dd919405
Clayton <cjdetke@yahoo.com>
2018-02-03 20:25:17 -0500

    Initial work on BambooAttack
    Added basic implementation of BambooAttack, now it is spawned as it should be.

--- Assets/C#/PlayerScripts/PlayerAbilities/BambooAttack/Ability_BambooSpawner.cs
+++ Assets/C#/PlayerScripts/PlayerAbilities/BambooAttack/Ability_BambooSpawner.cs
@@ -5,13 +5,35 @@ using UnityEngine;
 public class Ability_BambooSpawner : Ability_ObjectSpawner {
 
     public float bambooLifetime = 4;
+    public float bambooShootsSpawned = 5;
+    public float damage = 20;
+    public GameObject bambooShootToSpawn;
+    public float spreadMultiplier;
 
     public override void OnSpellSpawned(GameObject spawn)
     {
         Bamboo b;
         if(b = spawn.GetComponent<Bamboo>())
         {
-            b.StartBamboo(this, this.bambooLifetime);
+            b.StartBamboo(this, this.bambooLifetime, this.GetComponentInParent<PlayerStats>().gameObject);
         }
+    }
+
+    public override void SpawnSpell(PlayerComponent.Buf data)
+    {
+        Vector3 spawnPosition = data.vectorList[1];
+        // Spawn our spell in the place the server told us
+        // However if we are the client, we don't wait for that luxury.
+        for (int i = 0; i < bambooShootsSpawned; i++)
+        {
+            Vector3 t = new Vector3(0,0,0);
+            t.x += (float)((Random.Range(0.0f, 1.0f) - .5) * spreadMultiplier);
+            t.y += (float)((Random.Range(0.0f, 1.0f) - .5) * spreadMultiplier);
+            t.z += (float)((Random.Range(0.0f, 1.0f) - .5) * spreadMultiplier);
+            GameObject spawn = GameObject.Instantiate(bambooShootToSpawn, spawnPosition + transform.TransformDirection(spawnOffset) + t, transform.rotation);
+            Destroy(spawn, bambooLifetime);
+        }
+        GameObject newSpawn = GameObject.Instantiate(itemToSpawn, spawnPosition + transform.TransformDirection(spawnOffset), transform.rotation);
+        OnSpellSpawned(newSpawn);
     }
 }
--- Assets/C#/PlayerScripts/PlayerAbilities/BambooAttack/Bamboo.cs
+++ Assets/C#/PlayerScripts/PlayerAbilities/BambooAttack/Bamboo.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class Bamboo : MonoBehaviour {
 
+    public GameObject sourcePlayer;
     Ability_BambooSpawner ability;
+    public PlayerEffects.Effects effect;
 
-    public void StartBamboo(Ability_BambooSpawner ability, float cooldown)
+    public void StartBamboo(Ability_BambooSpawner ability, float cooldown, GameObject sourcePlayer)
     {
         this.ability = ability;
-        Destroy(this.gameObject, cooldown);
+        this.sourcePlayer = sourcePlayer;
+        Invoke("DestroyMe", cooldown);
+    }
+
+    void DestroyMe()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponentInParent<IHittable>() == null)
+            return;
+        PlayerStats ps;
+        if ((ps = col.GetComponentInParent<PlayerStats>()) != null && ps.gameObject == sourcePlayer.gameObject)
+        {
+            return;
+        }
+        else
+        {
+            Rigidbody r;
+            if ((r = col.transform.GetComponent<Rigidbody>()) != null)
+            {
+                if (ps)
+                {
+                    HitManager.HitClientside(new HitArguments(r.GetComponentInParent<BasePlayer>().gameObject, sourcePlayer).withDamage(ability.damage).withEffect(effect));
+                }
+            }
+        }
     }
 }
