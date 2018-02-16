e93baf6085a96840d749e36c85982b761b1f5c40
Clayton <cjdetke@yahoo.com>
2018-02-15 19:15:26 -0500

    Added basic GrassTrap spell
    Made a basic implementation of the GrassTrap spell, and nothing else.

---
+++ Assets/C#/PlayerScripts/PlayerAbilities/GrassTrap/Ability_GrassTrapSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability_GrassTrapSpawner : Ability_ObjectSpawner
+{
+
+    public float effectDuration = 3;
+    public float trapLifetime = 6;
+    public float damage = 20;
+
+    public override void OnSpellSpawned(GameObject spawn)
+    {
+        GrassTrap g;
+        if(g = spawn.GetComponent<GrassTrap>())
+        {
+            g.StartGrassTrap(this, this.trapLifetime, this.effectDuration, this.GetComponentInParent<PlayerStats>().gameObject);
+        }
+    }
+
+    public override void SpawnSpell(PlayerComponent.Buf data)
+    {
+        Vector3 spawnPosition = data.vectorList[1];
+
+        GameObject spawn = GameObject.Instantiate(itemToSpawn, spawnPosition + transform.TransformDirection(spawnOffset), transform.rotation);
+        OnSpellSpawned(spawn);
+    }
+}
---
+++ Assets/C#/PlayerScripts/PlayerAbilities/GrassTrap/GrassTrap.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassTrap : MonoBehaviour {
+
+    public GameObject sourcePlayer;
+    Ability_GrassTrapSpawner ability;
+    public PlayerEffects.Effects effect;
+    private float effectDuration = 3;
+    private ArrayList hasHit;
+
+    public void StartGrassTrap(Ability_GrassTrapSpawner ability, float effectDuration, float cooldown, GameObject sourcePlayer)
+    {
+        this.ability = ability;
+        this.sourcePlayer = sourcePlayer;
+        this.effectDuration = effectDuration;
+        hasHit = new ArrayList();
+        Invoke("DestroyMe", cooldown);
+    }
+
+    void DestroyMe()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponentInParent<IHittable>() == null)
+            return;
+        PlayerStats ps;
+        if ((ps = col.GetComponentInParent<PlayerStats>()) != null && ps.gameObject == sourcePlayer.gameObject)
+        {
+            return;
+        }
+        else
+        {
+            Rigidbody r;
+            if ((r = col.transform.GetComponent<Rigidbody>()) != null)
+            {
+                if (ps && !hasHit.Contains(ps))
+                {
+                    hasHit.Add(ps);
+                    HitManager.HitClientside(new HitArguments(r.GetComponentInParent<BasePlayer>().gameObject, sourcePlayer).withDamage(ability.damage).withEffect(effect).withEffectDuration(effectDuration));
+                    if(hasHit.Count <= 1)
+                    {
+                        Invoke("DestroyMe", effectDuration);
+                    }
+                }
+            }
+        }
+    }
+}
