9fe13178be386c74735c235e659e30b3b63c0e73
Michael Beshear <MEBeshear@forted.com>
2018-04-03 19:41:51 -0400

    Changed jumpPads to "BouncePads"
    Made it so that jumpPad adds force when stepped on to player, making them into "BouncePads"

--- Assets/C#/World/JumpPad.cs
+++ Assets/C#/World/JumpPad.cs
@@ -4,24 +4,40 @@ using UnityEngine;
 
 public class JumpPad : MonoBehaviour {
     public int jumpModifier;
+    private ArrayList onCooldown;
 
     void OnTriggerEnter(Collider col)
     {
         if (col.isTrigger) return;
-        PlayerMovement m;
-        if ((m = col.GetComponentInParent<PlayerMovement>()) != null)
+        BasePlayer b;
+        if ((b = col.GetComponentInParent<BasePlayer>()) != null && !onCooldown.Contains(b))
         {
-            m.jumpHeight += jumpModifier;
+            b.myRigid.AddForce(Vector3.up * b.myRigid.mass * jumpModifier);
+            onCooldown.Add(b);
         }
+        
     }
 
     void OnTriggerExit(Collider col)
     {
         if (col.isTrigger) return;
-        PlayerMovement m;
-        if ((m = col.GetComponentInParent<PlayerMovement>()) != null)
+        BasePlayer b;
+        if ((b = col.GetComponentInParent<BasePlayer>()) != null && onCooldown.Contains(b))
         {
-            m.jumpHeight -= jumpModifier;
+            onCooldown.Remove(b);
         }
+    }
+
+    public IEnumerator Cooldown(BasePlayer b)
+    {
+        do
+        {
+            // Local damage only
+            if ((Component)b != null && ((Component)b).gameObject != null)
+            {
+                yield return new WaitForSeconds(1);
+                onCooldown.Remove(b);
+            }
+        } while (b != null && onCooldown.Contains(b));
     }
 }
