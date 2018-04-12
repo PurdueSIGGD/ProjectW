fd787d3861c33a4d5671dab28831140e1bdebc47
Clayton <cjdetke@yahoo.com>
2018-04-12 19:46:42 -0400

    Updated flamethrower
    Now that druid was merged to master, flamethrower now immobilizes the player when casting it, and can only be cast while the player is on the ground.  Also, slightly increased flamethrower damage.

--- Assets/C#/PlayerScripts/PlayerAbilities/Flamethrower/Ability_FlamethrowerSpawner.cs
+++ Assets/C#/PlayerScripts/PlayerAbilities/Flamethrower/Ability_FlamethrowerSpawner.cs
@@ -9,6 +9,8 @@ public class Ability_FlamethrowerSpawner : Ability_ObjectSpawner {
     public float lifetime = 1;
     public PlayerEffects.Effects effect;
 
+    private float lastUsage = -100; // Last time we used it, in seconds;
+
     public override void OnSpellSpawned(GameObject spawn)
     {
         Flamethrower f;
@@ -41,6 +43,30 @@ public class Ability_FlamethrowerSpawner : Ability_ObjectSpawner {
 
         Destroy(flameSpawned, lifetime);
         PlayerStats sourcePlayerStats = GetComponentInParent<PlayerStats>();
-        HitManager.HitClientside(new HitArguments(sourcePlayerStats.gameObject, sourcePlayerStats.gameObject).withEffect(effect).withEffectDuration(cooldown));
+        HitManager.HitClientside(new HitArguments(sourcePlayerStats.gameObject, sourcePlayerStats.gameObject).withEffect(effect).withEffectDuration(cooldown+.2f));
+    }
+    public override void use()
+    {
+        // Clients should not worry about magic draw
+        if (Time.time - lastUsage > cooldown && !myBase.myMovement.isInAir())
+        {
+            if (!isLocalPlayer || myBase.myStats.canUseMagic(magicDraw))
+            {
+                if (abilityIcon != null && cooldown > 0.2f)
+                { // We don't let it go if it takes like no time
+                    this.abilityIcon.myAnimator.SetFloat("CooldownSpeed", 1 / cooldown);
+                    string extra = UnityEngine.Random.Range(0.0f, 1.0f) < 0.001 ? "1" : ""; // ;)
+                    this.abilityIcon.myAnimator.SetTrigger("Cooldown" + extra);
+                }
+                myBase.myStats.changeMagic(-1 * magicDraw);
+                lastUsage = Time.time;
+                use_UseAbility();
+            }
+            else
+            {
+                // Not enough magic
+            }
+
+        }
     }
 }
