8d386fd2d9a7ae21ee9d37ac505c260bf07c1427
Clayton <cjdetke@yahoo.com>
2018-04-21 18:56:54 -0400

    All balance changes but timewall one
    Debuffed druid's poison arrow's damage.
    Buffed druid's bamboo attack's range.
    Time speedup projectile now explodes on self instead of being a projectile.
    Buffed earth projectile's damage.
    Buffed earth player's slam attack's damage.
    Buffed earth player's stalagmite chain's push force.
    Flamethrower now slows movement speed by 50% instead of immobilizes the player.
    
    WHAT I STILL NEED TO BALANCE:  fix timewall/druid projectile problem.

---
+++ Assets/C#/PlayerScripts/PlayerEffects/Effect_SlowRunSpeed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect_SlowRunSpeed : Effect {
+    public float runSpeedScale = 0.5f;
+    public override void Effect_Start(PlayerEffects target)
+    {
+        target.runSpeedModifier *= runSpeedScale;
+    }
+
+    public override void Effect_End(PlayerEffects target)
+    {
+        target.runSpeedModifier /= runSpeedScale;
+    }
+
+    public override void Effect_Update()
+    {
+
+    }
+}
