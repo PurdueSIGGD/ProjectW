33e892bd5b5911abca911d4103d806acd0f7932d
yang132 <31993113+yang132@users.noreply.github.com>
2018-05-16 13:16:39 -0400

    Added random horizontal movement

--- Assets/C#/PlayerScripts/PlayerInputs/HumanInput.cs
+++ Assets/C#/PlayerScripts/PlayerInputs/HumanInput.cs
@@ -6,11 +6,13 @@ public class HumanInput : PlayerInput {
     public float sensitivityy = 0.05f;
 	public bool AI = true;
 	public bool multTeams = false;
+	private int[,] botHorizontals;
 
 	public override void PlayerComponent_Start() {
 		base.PlayerComponent_Start ();
 		if (isServer) {
 			ProjectWGameManager manager = FindObjectOfType<ProjectWGameManager> ();
+			botHorizontals = new int[manager.networkManager.botItems.Length,2];
 			if (manager.teams.Length > 1) {
 				multTeams = true;
 			}
@@ -47,7 +49,7 @@ public class HumanInput : PlayerInput {
 			data.scoreboard = false;
 			data.useAbilities = triggerAttack ();
 			data.melee = false;
-
+			
 			return data;
 
 	}
@@ -85,15 +87,36 @@ public class HumanInput : PlayerInput {
 
 		if (targetPosition.HasValue) {
 			
-			data.mouseX = -4 * Vector3.SignedAngle ((Vector3)targetPosition - cameraSlider.position, cameraSlider.forward, Vector3.up) * sensitivityy;
+			data.mouseX = -40 * Vector3.SignedAngle ((Vector3)targetPosition - cameraSlider.position, cameraSlider.forward, Vector3.up) * sensitivityy;
 
-			data.mouseY = 4 * Vector3.SignedAngle ((Vector3)targetPosition - cameraSlider.position, cameraSlider.forward, Vector3.Cross (cameraSlider.forward, Vector3.up)) * sensitivityy;
+			data.mouseY = 10 * Vector3.SignedAngle ((Vector3)targetPosition - cameraSlider.position, cameraSlider.forward, Vector3.Cross (cameraSlider.forward, Vector3.up)) * sensitivityy;
 			double dist = Vector3.Distance (cameraSlider.position, (Vector3)targetPosition);
 			if (dist > 4) {
 				data.vertical = 1f;
 			} else if (dist < 3) {
 				data.vertical = -1f;
 			}
+			System.Random rnd = new System.Random ();
+			botHorizontals [getBot () - 1,0] += rnd.Next (-1, 2);
+			if (botHorizontals [getBot () - 1,0] > 10) {
+				botHorizontals [getBot () - 1,0] = 10;
+
+			} else if (botHorizontals [getBot () - 1,0] < -10) {
+				botHorizontals [getBot () - 1,0] = -10;
+			}
+				
+
+			if (botHorizontals [getBot () - 1,0] > 7) {
+				botHorizontals [getBot () - 1,1] = 1;
+
+			} else if (botHorizontals [getBot () - 1,0] < -7) {
+				botHorizontals [getBot () - 1,1] = -1;
+
+			} else if (botHorizontals [getBot () - 1,0] > -3 && botHorizontals [getBot () - 1,0] < 3) {
+				botHorizontals [getBot () - 1,1] = 0;
+			}
+			data.horizontal = botHorizontals [getBot () - 1,1];
+
 		}
 
 
@@ -128,7 +151,7 @@ public class HumanInput : PlayerInput {
 				if (ignorePlayer (tmpSts))
 					continue;
 
-				useAbilities[1] = true;
+				//useAbilities[1] = true;
 				break;
 			}
 		}
@@ -146,9 +169,9 @@ public class HumanInput : PlayerInput {
 				if (ignorePlayer (tmpSts))
 					continue;
 
-				if (tmpSts.classIndex != 1) {//will fireball if target is not a time wizard
-					//useAbilities [0] = true;
-				}
+				//if (tmpSts.classIndex != 1) {//will fireball if target is not a time wizard
+					useAbilities [0] = true;
+				//}
 
 
 			} else {
